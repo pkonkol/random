b5fd26f432fb94c3ee1600a320ca49dd639f2245
pkonkol <piotrkonkol@pm.me>
2021-08-08 14:07:17 +0200

    Working demo of zmq communication with asyncio

--- eks_distributed_scraper/master/main.py
+++ eks_distributed_scraper/master/main.py
@@ -1,12 +1,63 @@
+import asyncio
 import zmq
+import zmq.asyncio
 import time
+
+from collections import deque
+proxies = deque(["1", "2", "3", "4"])
+
 print("Started master")
 
-context = zmq.Context()
-socket = context.socket(zmq.PUSH)
-socket.bind("tcp://*:5556")
+context = zmq.asyncio.Context()
 
-while True:
-    print("I'm alive")
-    socket.send_string(f"I'm alive {time.ctime()}")
-    time.sleep(3)
+async def push_tasks():
+    task_sock = context.socket(zmq.PUSH)
+    task_sock.bind("tcp://*:5556")
+    while True:
+        print("In push_tasks")
+        await task_sock.send_string(f"I'm alive {time.ctime()}")
+        await asyncio.sleep(13)
+
+async def recv_result():
+    result_sock = context.socket(zmq.PULL)
+    result_sock.bind("tcp://*:5558")
+    while True:
+        print("In recv_result")
+        msg = await result_sock.recv_string()
+        print(f"Recived response: {msg}")
+        await asyncio.sleep(1)
+
+async def return_proxy():
+    sock = context.socket(zmq.REP)
+    sock.bind("tcp://*:5557")
+    while True:
+        print("In return_proxy")
+        msg = await sock.recv_string()
+        print("Received proxy request")
+        await sock.send_string(f"proxy return from master: {proxies[0]}")
+        proxies.rotate(-1)
+
+
+async def parse_categories():
+    sitemap_url = "olx.pl/sitemap"
+    while True:
+        print("Main loop parsing")
+        await asyncio.sleep(10)
+
+def create_tasks():
+    tasks = []
+    tasks.append(asyncio.create_task(push_tasks()))
+    tasks.append(asyncio.create_task(recv_result()))
+    tasks.append(asyncio.create_task(return_proxy()))
+    tasks.append(asyncio.create_task(parse_categories()))
+    return tasks
+
+async def main():
+    await asyncio.gather(*create_tasks())
+
+
+if __name__ == "__main__":
+    # loop = asyncio.get_event_loop()
+    print('in __main__')
+    asyncio.run(main())
+
--- eks_distributed_scraper/slave/main.py
+++ eks_distributed_scraper/slave/main.py
@@ -4,16 +4,39 @@ import time
 import zmq
 print("Started slave")
 
-ZMQ_MASTER_HOSTNAME = os.environ.get("ZMQ_MASTER_HOSTNAME")
+ZMQ_MASTER_HOSTNAME = os.environ.get("ZMQ_MASTER_HOSTNAME", 'localhost')
 HOSTNAME = os.environ.get("HOSTNAME")
+PROXY = None
+
+# SOCKS_PROXY = None
 
 context = zmq.Context()
-socket = context.socket(zmq.PULL)
-master_addr=f"tcp://{ZMQ_MASTER_HOSTNAME}:5556"
-print(master_addr)
-socket.connect(master_addr)
+task_sock = context.socket(zmq.PULL)
+task_sock.connect(f"tcp://{ZMQ_MASTER_HOSTNAME}:5556")
+
+prox_sock = context.socket(zmq.REQ)
+prox_sock.connect(f"tcp://{ZMQ_MASTER_HOSTNAME}:5557")
+
+result_sock = context.socket(zmq.PUSH)
+result_sock.connect(f"tcp://{ZMQ_MASTER_HOSTNAME}:5558")
+
+
+def get_proxy():
+    print("sending proxy request")
+    prox_sock.send_string("REQUESTING SOCKS PROXY")
+    print("sent proxy request")
+    return prox_sock.recv_string()
+
 
 while True:
-    # print("I'm alive")
-    print(f"Pulled package: {socket.recv_string()} on {HOSTNAME}")
+    print(f"Pulled task: {task_sock.recv_string()} on {HOSTNAME}")
     time.sleep(random.random()*10)
+    if (not PROXY):
+        PROXY = get_proxy()
+        print(f"received proxy: {PROXY}")
+    time.sleep(random.random()*5)
+    result_sock.send_string("Returning result to master")
+    print("returned result")
+    if (random.random() < 0.1):
+        PROXY = None
+        print("destroying proxy")
