1b1665dd7310bd87a86562afe8ffaa2eddd0fab4
pkonkol <piotrkonkol01@gmail.com>
2022-10-15 18:25:20 +0200

    removed homelab and renamed scraper

--- eks_distributed_scraper/cloud/provider.tf
+++
@@ -1,7 +0,0 @@
-provider "aws" {
-  region = var.AWS_REGION
-
-  default_tags {
-    tags = var.DEFAULT_TAGS
-  }
-}
--- homelab/Readme.md
+++
@@ -1,4 +0,0 @@
-# Usage
-
-`ansible-playbook -l all -i hosts provision_ssh_keys.yml`
-`ansible x86 -i hosts -m ping`
